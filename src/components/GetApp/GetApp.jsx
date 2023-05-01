df6d8d48a6d97007a1d7265b83b3acc1d3bc22db
Salem <selem.menssi.2000@gmail.com>
2023-05-01 08:42:41 +0100

    modify

--- src/components/GetApp/GetApp.jsx
+++ src/components/GetApp/GetApp.jsx
@@ -7,6 +7,8 @@ const GetApp = () => {
   return (
     <div id="GetApp" className="GetApp">
       <h2>Get the App !</h2>
+      <span />
+      <p>To enjoy our services and products download the app</p>
       <div className="btn-box">
         <a href="#">
           <img src={playStore} alt="play Store" />
